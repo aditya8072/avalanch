c1e6a00c0e9784f59f3725ed2f52263cedcb0ff8
aditya8072 <114757289+aditya8072@users.noreply.github.com>
2023-07-21 17:10:10 +0530

    Create module.sol

---
+++ module.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract ErrorHandlingExample {
+    uint256 public totalBalance;
+
+    function deposit(uint256 amount) public {
+        require(amount > 0, "Enter the amount greater than zero");
+        totalBalance += amount;
+    }
+
+    function withdraw(uint256 amount) public {
+        require(amount > 0, "Enter the insufficent balance");
+        require(amount <= totalBalance, "Insufficient balance");
+
+        totalBalance -= amount;
+    }
+
+    function divide(uint256 a, uint256 b) public pure returns (uint256) {
+        require(b != 0, "Divisor must be non-zero");
+
+        return a / b;
+    }
+
+    function assertExample(uint256 value) public pure returns (uint256) {
+        assert(value > 0);
+        return value;
+    }
+}
